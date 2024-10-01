d012e4494443a8d660b4f2c3380d18a4968d6ce6
Javier David Jimenez <jadajima@gmail.com>
2024-10-01 12:57:00 +0200

    He creado unos cuantas carpetas y Los 3 pimeros ejercicios de ejercicios.js Tema1 donde iran todos juntos

---
+++ core-javascript/ejercicios/tema1/ejercicios.js
@@ -0,0 +1,16 @@
+// 1 Declara una variable normal y una constante
+
+let x = 25;
+const Nombre = "David";
+
+// 2 Declara una variable normal. Asígnala después a un número
+
+let Num;
+Num = 25;
+
+
+//Declara dos variables numéricas y declara una tercera con su suma
+
+let num1 = 12;
+let num2 = 13;
+let sumado = num1 + num2;
