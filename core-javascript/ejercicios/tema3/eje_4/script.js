e5273f07d24c802df759b8087e5a6e7568eb4d8a
Javier David Jimenez <jadajima@gmail.com>
2024-10-05 14:18:03 +0200

    Acabamos el tem 3 ejercicio 4

--- core-javascript/ejercicios/tema3/eje_4/script.js
+++ core-javascript/ejercicios/tema3/eje_4/script.js
@@ -1,27 +1,27 @@
-function analiza_ruedas() {
+function analyze_wheels() {
    
-    let diametro = parseFloat(document.getElementById("diametro").value);
-    let grosor = parseFloat(document.getElementById("grosor").value);
-    let diagnostico = "";
+    let diameter = parseFloat(document.getElementById("diameter").value);
+    let thickness = parseFloat(document.getElementById("thickness").value);
+    let  answer = "";
 
    
 
-    if (diametro <= 0 || grosor <= 0) {
-        diagnostico = "Si el diametro o el grosor de la rueda es 0 o menor esa rueda no existe"
-    } else if (diametro > 1.4) {
-        diagnostico = "La rueda es para un vehículo grande.";  
-        if (grosor < 0.4) {
-            diagnostico += " El grosor para esta rueda es inferior al recomendado.";
+    if (diameter <= 0 ||  thickness <= 0) {
+         answer = "Si el diametro o el grosor de la rueda es 0 o menor esa rueda no existe"
+    } else if (diameter > 1.4) {
+         answer = "La rueda es para un vehículo grande.";  
+        if (thickness < 0.4) {
+             answer += " El grosor para esta rueda es inferior al recomendado.";
         }
-    } else if (diametro > 0.8 && diametro <= 1.4) {
-        diagnostico = "La rueda es para un vehículo mediano.";
-        if (grosor < 0.25) {
-            diagnostico += " El grosor para esta rueda es inferior al recomendado.";
+    } else if (diameter > 0.8 && diameter <= 1.4) {
+         answer = "La rueda es para un vehículo mediano.";
+        if (thickness < 0.25) {
+             answer += " El grosor para esta rueda es inferior al recomendado.";
         }
     } else {
-        diagnostico = "La rueda es para un vehículo pequeño.";
+         answer = "La rueda es para un vehículo pequeño.";
     }
 
 
-    document.getElementById("resultado").innerHTML = diagnostico;
+    document.getElementById("result").innerHTML =  answer;
 }
