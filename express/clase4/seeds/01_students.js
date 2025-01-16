a8da602143e70cb60fa2e07ff209c24b0828a83a
Javier David Jimenez <jadajima@gmail.com>
2025-01-16 13:23:48 +0100

    rehacemos student borrandolo y volviendolo a hacer ya que en la migration de creación se llamaba last name y no last_name lo cual daba errores

--- express/clase4/seeds/01_students.js
+++ express/clase4/seeds/01_students.js
@@ -4,10 +4,12 @@
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+    await knex('students' ).del();
+    await knex('students' ).insert([
+    {name: 'Hugo', last_name: 'López', date_of_birth: '2018-10-01' },
+    {name: 'María', last_name: 'Jiménez', date_of_birth: '1982-11-04' },
+    {name: 'Asier', last_name: 'Valencia' , date_of_birth: '2019-05-12' }
+    ]);
+   // await knex.raw('select setval(\'students_id_seq\', max(id)) from
+   //students'); 
 };
