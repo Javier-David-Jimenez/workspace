3402abf3b9d44498e6ba6711009a6e553ce7b72c
Javier David Jimenez <jadajima@gmail.com>
2025-01-22 18:07:12 +0100

    primeros cambios y login.html

--- express/clase6/app.js
+++ express/clase6/app.js
@@ -2,8 +2,46 @@
 const express = require("express");
 // Creo la aplicación Express
 const app = express();
+//añadimos cookie-parser
+const cookieParser = require("cookie-parser");
 // Declaro el puerto de escucha
 const port = 3000;
+
+
+
+app.use(cookieParser("ClaveSuperSecreta"));
+
+
+
+// Ruta para obtener los valores de las cookies
+app.get("/cookies" , (req, res) => {
+// Cookies que no han sido firmadas
+  console. log("Cookies: " , req.cookies);
+// Cookies que han sido firmadas
+  console. log("Signed Cookies: " , req.signedCookies);
+res.json({
+  customCookie: req.cookies.customCookie,
+  customSignedCookie: req.signedCookies.customSignedCookie,
+  });
+});
+// Ruta protegida, necesita que la variable haya sidoconfigurada
+app.get("/protected" , (req, res) => {
+  if (req.cookies.customCookie) {
+    res.send("Cookie has been set!" );
+  } else {
+    res.send("The Cookie doesn't exist!" )
+  }
+});
+
+
+
+
+
+
+
+
+
+
 // Defino la ruta que se llamará cuando se reciba una petición HTTP GET
 // en la dirección '/'
 // La función callback recibe una petición y una respuesta como argumentos
